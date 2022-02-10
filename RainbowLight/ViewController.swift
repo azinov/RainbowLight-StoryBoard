01d50fd501ecf21f17fbc5e1d419d10cd23912f1
Alexey Zinovyev <zinovkin@gmail.com>
2022-02-11 02:13:11 +0300

    Add array of UIColor

--- RainbowLight/ViewController.swift
+++ RainbowLight/ViewController.swift
@@ -10,8 +10,12 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    var viewColor = 1
+    /// Set the color of view
+    var viewColor = 0
+    var colorUI = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor(red: 128/255, green: 166/255, blue: 255/255, alpha: 1.0), UIColor.blue, UIColor.purple]
+    var touchCount = 1
     
+    /// Hide status bar
     override var prefersStatusBarHidden: Bool {
         return true
     }
@@ -25,35 +29,31 @@ class ViewController: UIViewController {
         
     fileprivate func switchUpdateUI() {
 
+        viewColor += 1
+
         switch viewColor {
-        
+
         case 1:
             view.backgroundColor = .red // Красный
-            viewColor += 1
             toggleTorch(on: true, level: 0.1)
         case 2:
             view.backgroundColor = .orange // Оранжевый
-            viewColor += 1
             toggleTorch(on: true, level: 0.2)
         case 3:
             view.backgroundColor = .yellow // Желтый
-            viewColor += 1
             toggleTorch(on: true, level: 0.3)
         case 4:
             view.backgroundColor = .green // Зеленый
-            viewColor += 1
             toggleTorch(on: true, level: 0.4)
         case 5:
             view.backgroundColor = UIColor(red: 128/255, green: 166/255, blue: 255/255, alpha: 1.0) // Голубой или .cyan
-            viewColor += 1
             toggleTorch(on: true, level: 0.5)
         case 6:
             view.backgroundColor = .blue // Синий
-            viewColor += 1
             toggleTorch(on: true, level: 0.6)
         default:
             view.backgroundColor = .purple // Фиолетовый
-            viewColor = 1
+            viewColor = 0
             toggleTorch(on: true, level: 0.7)
             
         }
@@ -79,8 +79,16 @@ class ViewController: UIViewController {
     // MARK: - Methods
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        switchUpdateUI()
+        // Use switch
+        //        switchUpdateUI()
+        
+        // Use array of colorUI
+        
+        touchCount = touchCount <= colorUI.count-1 ? touchCount : 0
+        view.backgroundColor = colorUI[touchCount]
+        touchCount += 1
+        
     }
-      
+    
 }
 
